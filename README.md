75492a6f598340daf9dc859a281e1f08e98bc7d0
margaretau <aumarg.edu@gmail.com>
2022-04-18 16:40:46 -0700

    attempted to fetch from different APIs and present dynamic data on the page, a work in progress

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 ARTDM174 Repository
+
+API Sources:
+
+I. General info:
+https://www.programmableweb.com/api/index-sciences-rest-api-v10
+https://www.indexofsciences.com/about-us/
+
+general posting:
+    https://www.indexofsciences.com/index.php/wp-json/wp/v2/posts
+page and specific topic:
+see under content.rendered for the full page code
+    https://www.indexofsciences.com/index.php/wp-json/wp/v2/posts?page=1&search=diabetes
+
+documentation:
+    https://www.indexofsciences.com/wp-json/wp/v2
+
+feature articles:
+    https://www.indexofsciences.com/13-ways-for-preventing-type-2-diabetes/
+
+    https://www.indexofsciences.com/good-and-bad-drinks-for-people-with-diabetes/
+
+
+
+II. General Info:
+https://www.programmableweb.com/api/food-rest-api-v10
+https://www.programmableweb.com/api/food-rest-api-v10
+https://spoonacular.com/food-api
+https://spoonacular.com/food-api/docs#Quotas
+https://spoonacular.com/food-api/docs
+Look at the endpoints for Meal Planning
+
+search recipes by ingredients:
+documentation:
+    https://spoonacular.com/food-api/docs#Search-Recipes-by-Ingredients
+
+    fetch ("https://api.spoonacular.com/recipes/findByIngredients")
+
+https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=API-KEY
+
+III. Fun APIs
+Random Useless Facts:
+    https://uselessfacts.jsph.pl/random.json?language=en
+
+Techy Phrases:
+    https://techy-api.vercel.app/api/json
--- js/main.js
+++ js/main.js
@@ -1,30 +1,100 @@
 
 //start by loading one feature article on the page
-    document.addEventListener("DOMContentLoaded", (e) => {
-        let featurePage = document.getElementById("feature-article");
-        console.log(featurePage);
-        featurePage.src = "https://www.indexofsciences.com/good-and-bad-drinks-for-people-with-diabetes/";
+
+const featurePage = document.getElementById("feature-article");
+document.addEventListener("DOMContentLoaded", (e) => {
+        featurePage.src = "https://www.indexofsciences.com/13-ways-for-preventing-type-2-diabetes/";
     });
 
-/*Function to gather the article json information
-async function getArticles() {
+//Fun Section: Random Fact
+async function getFact() {
     try {
-        let response = await fetch("houses.json");
+        let response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
         let data = await response.json();
         return data;
     } catch (err) {
         console.log('There is an error', err);
     }
 }
+async function presentFact() {
+    try {
+        let randomFact = await getFact();
+        fact = randomFact.text;
+        console.log(fact);
+        const factoid = document.querySelector("#factoid");
+        factoid.innerHTML = fact;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
+}
+
+//Fun Section: Techy Talk
+async function getTech() {
+    try {
+        let response = await fetch(" https://techy-api.vercel.app/api/json");
+        let data = await response.json();
+        return data;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
+}
+async function presentTech() {
+    try {
+        let randomTech = await getTech();
+        techMsg = randomTech.message;
+        console.log(techMsg);
+        const techy = document.querySelector("#techy");
+        techy.innerHTML = techMsg;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
+}
+
+presentFact();
+presentTech();
+
+
+
+// Fetch and display the new article feed
+
+const articles="";
+const articleLink = "";
+const articleNumber = 0;
+/*
+processArticles(articles);
+console.log('articles json:', articles);
+articleLink = articles[articleNumber].link;
+console.log('new url', articleLink)
+featurePage.src = articleLink;
 */
+/*
+//Function to gather the article json information
+async function getArticles() {
+    try {
+        let response = await fetch("https://www.indexofsciences.com/index.php/wp-json/wp/v2/posts");
+        let data = await response.json();
+        return data;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
+}
 
-/* function to post the feature article on the page 
-function processArticles() {
-
-        let articles = await getArticles();
-        let articleLink = "";
-        let articleNumber = 0;
-
+// function to post the feature article on the page 
+async function processArticles(articles) {
+    try {
+        articles = await getArticles();
+        console.log('articles json:', articles);
+        articleLink = articles[articleNumber].link;
+        console.log('new url', articleLink);
+        featurePage.src = articleLink;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
+}
+*/
+    //    let articleLink = "";
+    //    let articleNumber = 0;
+/*
         houses.forEach((house) => {
             let objInfo = `<dt class="house">${house.name}</dt>`;
             house.members.forEach((member) => {
@@ -45,3 +115,7 @@ function processArticles() {
     //catch (err) {console.log('There is an error', err)};
      //  processArticles(0);
     */
+
+
+
+//Detect for request to change the article search criteria
