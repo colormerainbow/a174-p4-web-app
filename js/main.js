b0b9207ae9f3031d1286d667c87ce6778f5c1b51
margaretau <aumarg.edu@gmail.com>
2022-04-19 01:00:04 -0700

    added functional code for advancing articles, page init, and placeholder page for meal planning

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 
 //start by loading one feature article on the page
 
+document.addEventListener("DOMContentLoaded", init);
 const featurePage = document.getElementById("feature-article");
-document.addEventListener("DOMContentLoaded", (e) => {
-        featurePage.src = "https://www.indexofsciences.com/13-ways-for-preventing-type-2-diabetes/";
-    });
+let articles=[];
+let articleNumber = 0;
+const articlesPerPage = 2;
 
-//Fun Section: Random Fact
+/* set up the page content with the feeds */
+function init() {
+    // Fetch and display the new article feed
+    processArticles(1);
+    presentFact();
+    presentTech();
+
+    /* create shourtcut vars */
+    const prevArticle = document.querySelector(".back");
+    const nextArticle = document.querySelector(".next");
+
+     /*Detect when user requests to change the article */
+     prevArticle.addEventListener("click", changeArticle);
+     nextArticle.addEventListener("click", changeArticle);
+}
+//Fun Section: Fetch Random Fact
 async function getFact() {
     try {
         let response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
@@ -20,7 +36,6 @@ async function presentFact() {
     try {
         let randomFact = await getFact();
         fact = randomFact.text;
-        console.log(fact);
         const factoid = document.querySelector("#factoid");
         factoid.innerHTML = fact;
     } catch (err) {
@@ -28,7 +43,7 @@ async function presentFact() {
     }
 }
 
-//Fun Section: Techy Talk
+//Fun Section: Fetch Techy Talk
 async function getTech() {
     try {
         let response = await fetch(" https://techy-api.vercel.app/api/json");
@@ -42,7 +57,6 @@ async function presentTech() {
     try {
         let randomTech = await getTech();
         techMsg = randomTech.message;
-        console.log(techMsg);
         const techy = document.querySelector("#techy");
         techy.innerHTML = techMsg;
     } catch (err) {
@@ -50,28 +64,12 @@ async function presentTech() {
     }
 }
 
-presentFact();
-presentTech();
-
-
-
-// Fetch and display the new article feed
-
-const articles="";
-const articleLink = "";
-const articleNumber = 0;
-/*
-processArticles(articles);
-console.log('articles json:', articles);
-articleLink = articles[articleNumber].link;
-console.log('new url', articleLink)
-featurePage.src = articleLink;
-*/
-/*
 //Function to gather the article json information
-async function getArticles() {
+async function getArticles(page) {
     try {
-        let response = await fetch("https://www.indexofsciences.com/index.php/wp-json/wp/v2/posts");
+        let api = `https://www.indexofsciences.com/index.php/wp-json/wp/v2/posts?per_page=${articlesPerPage}&page=${page}`;
+        console.log('api is', api);
+        let response = await fetch(api);
         let data = await response.json();
         return data;
     } catch (err) {
@@ -80,42 +78,49 @@ async function getArticles() {
 }
 
 // function to post the feature article on the page 
-async function processArticles(articles) {
+async function processArticles(page) {
     try {
-        articles = await getArticles();
-        console.log('articles json:', articles);
-        articleLink = articles[articleNumber].link;
-        console.log('new url', articleLink);
-        featurePage.src = articleLink;
+        let newPage = await getArticles(page);
+        console.log(newPage);
+        articleNumber = articles.length;
+        articles = articles.concat(newPage);
+        console.log('articles new page is', articles);
+
+        console.log('array postion is', articleNumber);
+        assignArticle(articleNumber);
     } catch (err) {
         console.log('There is an error', err);
     }
 }
-*/
-    //    let articleLink = "";
-    //    let articleNumber = 0;
-/*
-        houses.forEach((house) => {
-            let objInfo = `<dt class="house">${house.name}</dt>`;
-            house.members.forEach((member) => {
-                objInfo += `<dd class="folks">${member}</dd>`;
-            });
 
-            // generate the html snippet for one array item and add to the "html" temp holder.
-            html += objInfo;
-        });
-        //close the html with the closing tag
-        html += `</dl>`;
+function assignArticle(n){
+    featurePage.src = articles[n].link;
+    console.log('featurePage', articleNumber);
+}
 
-        //make a reference to the html container where
-        //the info will be displayed.
-        const container = document.querySelector("#container");
-        container.innerHTML = html;
-    } 
-    //catch (err) {console.log('There is an error', err)};
-     //  processArticles(0);
-    */
+function changeArticle(e) {
+    // stop link from trying to reload page
+    e.preventDefault();
 
+    // check if we are at the end of the articles collection
 
+    if (e.target.className ==='back') {
+        if ((articleNumber === 0)) {
+            alert('No more articles');
+        } else {
+            articleNumber--;
+            assignArticle(articleNumber);
+        }
+    } else {
+        console.log('forward,', articleNumber, articleNumber === (articles.length-1))
+        if (articleNumber === (articles.length -1)) {
+            page = (Math.floor(articles.length/articlesPerPage) + 1);
+            processArticles(page);
+        } else {
+            articleNumber++;
+            assignArticle(articleNumber);
+        }
+    }
+}
 
-//Detect for request to change the article search criteria
+//Detect for request to change the article topic search criteria
