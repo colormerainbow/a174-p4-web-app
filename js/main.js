3ee702c0e2f14a17f150221b053adb4fc9cf41a9
margaretau <aumarg.edu@gmail.com>
2022-05-20 00:49:34 -0700

    code for menus module - fetch, indexdb store, dynamic js to render recipes, still work in progress

--- js/main.js
+++ js/main.js
@@ -4,19 +4,16 @@ document.addEventListener("DOMContentLoaded", init);
 const featurePage = document.getElementById("feature-article");
 const prevArticle = document.querySelector(".back");
 const nextArticle = document.querySelector(".next");
-let articleNumber = 1;
-// set the number of ariticles fetched for each API call, up to max 10
+
+var articleNumber = 1;
+// set the number of items fetched for each API call, up to max 10 for articles
 const articlesPerPage = 3;
+const numRecipe = 4;
 
-// create new indexedDB database
+
+// create new indexedDB databases
 var db = new Dexie("HealthNewsDB");
-
-/* regularly check if internet connectivity is present 
-setInterval(function(){
-    var status = navigator.onLine ? 'You are online!' : 'You are offline!';
-    console.log(status);
-}, 5000); 
-*/
+var recipedb = new Dexie("RecipesDB");
 
 //detect if go offline, remove NEXT button if at end of db stored articles
 window.addEventListener('offline', function (e) {
@@ -36,10 +33,10 @@ window.addEventListener('online', function (e) {
     nextArticle.classList.remove("hide");
 });
 
-/* set up the page content with the feeds */
+/* set up the initial page content */
 function init() {
 
-    // initialize the database schema
+    // initialize the database schema for health articles
     dbInit();
     // Display the initial article feed from db loaded from previous session or fetch if db not populated
     db.news.count((dbCount) => {
@@ -49,12 +46,26 @@ function init() {
             processArticles(1);
         }
     });
+
+    // initialize the database schema for recipes
+    recipedbInit();
+    // Initially display the recipes from db loaded from previous session or fetch if db not populated
+    recipedb.recipes.count((recipedbCount) => {
+        if (recipedbCount > 0) {
+            postRecipe(1);
+        } else {
+            processRecipe();
+        }
+    });
+
     presentFact();
     presentTech();
 
     /*Detect when user requests to change the article */
     prevArticle.addEventListener("click", changeArticle);
     nextArticle.addEventListener("click", changeArticle);
+
+
 }
 
 // function to initialize the indexedDB for health news articles
@@ -63,6 +74,13 @@ function dbInit() {
         news: "++, &link, title.rendered, content.rendered"
     });
 }
+// function to initialize the indexedDB for recipes
+function recipedbInit() {
+    recipedb.version(1).stores({
+        recipes: "++, &id, title, sourceUrl, image, nutrition.nutrients, spoonacularSourceUrl"
+    });
+}
+
 
 //Fun Section: Fetch Random Fact
 async function getFact() {
@@ -106,7 +124,7 @@ async function presentTech() {
     }
 }
 
-//Function to gather and present the health article json information
+//Function to fetch the health article json information
 async function getArticles(page) {
     try {
         let api = `https://www.indexofsciences.com/index.php/wp-json/wp/v2/posts?per_page=${articlesPerPage}&page=${page}`;
@@ -119,7 +137,7 @@ async function getArticles(page) {
     }
 }
 
-// function to post the feature article on the page 
+// overall function to process a feature article 
 async function processArticles(page) {
     try {
         let newPage = await getArticles(page);
@@ -205,11 +223,11 @@ async function changeArticle(e) {
 /* Nutrition - Meal Planning Section */
 
 //set variables based on user selection
-const diet = document.getElementById('diet');
-const allergy = document.getElementById('allergy');
-const findRecipe = document.getElementById('recipe');
+var diet = document.getElementById('diet');
+var allergy = document.getElementById('allergy');
+var findRecipe = document.getElementById('recipe');
 
-findRecipe.addEventListener('click', getRecipe);
+findRecipe.addEventListener('click', processRecipe);
 
 async function getRecipe() {
     var dietType = diet.options[diet.selectedIndex].text;
@@ -219,5 +237,119 @@ async function getRecipe() {
     document.getElementById("temp").innerHTML = dietType + exclude;
     console.log('Exclude from recipe:', exclude);
 
-    //run the fetch command and display recipe options
+    var dietFilter = "";
+    var excludeFilter = "";
+    if (dietType) {
+        dietFilter=`&diet=${dietType}`;
+    }
+    if (exclude) {
+        excludeFilter = `&intolerances=${exclude}`;
+    }
+    
+    //run the fetch command with user preferences and display recipe options
+ 
+    try {
+    //    let menuApi = `https://api.spoonacular.com/recipes/complexSearch?minProtein=20&maxSodium=500&maxSaturatedFat=3&minFiber=8${excludeFilter}${dietFilter}&addRecipeNutrition=true&number=${numRecipe}&apiKey=${myStuff}`;
+        let menuApi = `js/menustarter.json`; 
+        console.log('menuApi is', menuApi);
+        let menuData = await fetch(menuApi);
+        console.log('menuData is', menuData);
+        let menus = await menuData.json();
+        console.log('menus are', menus);
+
+        const menuArray = menus.results;
+        console.log('api data fetched is:', menuArray);
+        return menuArray;
+    } catch (err) {
+        console.log('There is an error with getRecipe', err);
+    }
+
 }
+
+async function processRecipe() {
+    try {
+        let newRecipes = await getRecipe();
+        console.log('new recipes are', newRecipes);
+
+        // load or add content into db for backup if lose internet connection
+        recipedb.recipes.bulkPut(newRecipes);
+        console.log('recipes loaded into indexedDB');
+        postRecipe(1);
+
+    } catch (err) {
+        console.log('There is an error with processArticles', err);
+    }
+}
+
+async function postRecipe(n) {
+   // var rbox2 = document.getElementById('recipe2box');
+   const recipeList = document.getElementById('recipeList');
+    try {
+        recipedb.recipes.each((recipe) => {
+            console.log("recipe is", recipe);
+            const li = document.createElement("LI");
+            var h4 = document.createElement("H4");
+            h4.innerHTML = recipe.title;
+            console.log(recipe.title,'is the recipe name');
+            var image = document.createElement("IMG");
+            image.src = recipe.image;
+            var rUrl = document.createElement("A");
+            rUrl.href = recipe.spoonacularSourceUrl;
+            rUrl.innerHTML = "Show Recipe";
+            rUrl.classList.add("menubtn");
+            recipeList.append(li, h4, rUrl, image);
+            });
+    
+        }catch (err) {
+            console.log('There is an error with postRecipe', err);
+        };
+    }
+        /*
+
+                      films.forEach((film) => {
+                const li = document.createElement("LI");
+                li.textContent = film.title;
+                movies_list.append(li);
+              });
+        console.log('The recipe number is:', n, 'recipeList is ',recipeList);
+        document.getElementById('recipe1').innerHTML = recipeList[n-1].title;
+        console.log('recipe name is', recipeList[n-1].title);
+
+        document.getElementById('recipe1img').src = recipeList[n-1].image;
+        
+        if (n < recipeList.length) {
+            n++;
+            rbox2.classList.remove("hide-all");
+            document.getElementById('recipe2').innerHTML = recipeList[n-1].title;
+            console.log('2nd recipe name is', recipeList[n-1].title);
+        
+            document.getElementById('recipe2img').src = recipeList[n-1].image;
+        } else {
+            rbox2.classList.add("hide-all");
+        }
+        */
+
+/*
+    if (navigator.onLine) {
+        featurePage.classList.remove('hide-all');
+        featurePage.src = article.link;
+        //clear any previous offline text
+        document.getElementById('offline-article').innerHTML = "";
+        console.log('featurePage via url-link, article no.', n, article.title.rendered);
+    } else {
+        document.getElementById('offline-article').innerHTML = ('<h2>' + article.title.rendered + '</h2>' + article.content.rendered);
+        //clear previous displayed article and remove iframe borders
+        featurePage.src = "";
+        featurePage.classList.add("hide-all");
+        console.log('featurePage via db, article no.', n, article.title.rendered);
+    }
+}*/
+
+
+
+/* regularly check if internet connectivity is present 
+setInterval(function(){
+    var status = navigator.onLine ? 'You are online!' : 'You are offline!';
+    console.log(status);
+}, 5000); 
+*/
